3752b6c290ab87a87647b4d355dd9946f65d12ac
iamaparnad <94278699+iamaparnad@users.noreply.github.com>
2022-03-06 07:23:57 +0530

    online exam

---
+++ online exam/quiz.js
@@ -0,0 +1,12 @@
+function result(){ 
+let score=0 
+if(document.getElementById('q13').checked){ 
+score+=1 
+} 
+if(document.getElementById('q22').checked){ 
+score+=1 } 
+if(document.getElementById('q31').checked){ 
+score+=1 
+} 
+alert("You've completed the Quiz!" + '\n' + "Your score: "+score+" of 3") 
+}
